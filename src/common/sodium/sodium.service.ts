aae8e3f040704d236be5ebadb07731965ceda527
Tangram ($TGM) <dev@getsneak.org>
2018-08-27 15:05:19 +0200

    Added Sodium service

---
+++ src/common/sodium/sodium.service.ts
@@ -0,0 +1,41 @@
+import * as sodium from 'sodium-native';
+import * as alloc from 'buffer-alloc-unsafe';
+
+export class SodiumService {
+
+    public hash_pwd(password: string, seed: string) {
+        const out = Buffer.alloc(64);
+        const passwd = Buffer.from(password);
+        const salt = Buffer.alloc(sodium.randombytes_SEEDBYTES, seed);
+        const opslimit = sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE;
+        const memlimit = sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE;
+        const algo = sodium.crypto_pwhash_ALG_DEFAULT;
+
+        sodium.crypto_pwhash(out, passwd, salt, opslimit, memlimit, algo);
+        return out;
+    }
+
+    public random_seed() {
+        const seed = Buffer.allocUnsafe(sodium.randombytes_SEEDBYTES);
+        
+        sodium.randombytes_buf(seed);
+        return seed;
+    }
+
+    public random_pwd() {
+        const buf = Buffer.alloc(16);
+        const seed = Buffer.allocUnsafe(sodium.randombytes_SEEDBYTES);
+
+        sodium.randombytes_buf(seed);
+        sodium.randombytes_buf_deterministic(buf, seed);
+        return buf;
+    }
+    
+    public generic_hash(value: string) {
+        const buf = Buffer.from(value);
+        const out = Buffer.alloc(sodium.crypto_generichash_BYTES);
+
+        sodium.crypto_generichash(out, buf);
+        return out;
+    }
+}
