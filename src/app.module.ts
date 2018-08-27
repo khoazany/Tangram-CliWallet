9e4fd054b8224bea5027a33fd920d75736501029
Tangram ($TGM) <dev@getsneak.org>
2018-08-27 15:05:54 +0200

    Added Sodium service provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,13 @@ import { SeedService } from './kadence/seed.service';
 import { EjectService } from './kadence/eject.service';
 import { QuasarService } from './kadence/quasar.service';
 import { Kadence } from './kadence/kadence.service';
-import { LmdbService } from 'common/database/lmdb.service';
+import { LmdbService } from './common/database/lmdb.service';
+import { SodiumService } from './common/sodium/sodium.service';
 
 @Module({
   providers: [
     Settings,
+    SodiumService,
     LmdbService,
     Kadence,
     EjectService,
