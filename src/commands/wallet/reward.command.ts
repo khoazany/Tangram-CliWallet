ca1f3e223febebaf184ff0dd9ffd01f00b620d7d
Tangram ($TGM) <dev@getsneak.org>
2018-09-13 10:31:44 +0200

    Added wallet reward

--- src/commands/wallet/reward.command.ts
+++ src/commands/wallet/reward.command.ts
@@ -6,6 +6,10 @@ import request = require('request');
 import Agent = require('socks5-http-client/lib/Agent');
 import { API } from "../../common/utils/api";
 import { ModuleRef } from "@nestjs/core";
+import { Kadence } from "../../kadence/kadence.service";
+import { Topic } from "../../common/enums/topic.enum";
+import { MessageEntity } from "../../common/database/entities/message.entity";
+import { MemberEntity } from "../../common/database/entities/member.entity";
 
 export class WalletRewardCommand extends Command {
     public register(vorpal: any): void {
@@ -19,32 +23,34 @@ export class WalletRewardCommand extends Command {
 
 export class WalletRewardReceiver implements IReceiver {
     private _settings: Settings;
+    private _kadence: Kadence;
 
     constructor(private readonly _app: ModuleRef) {
         this._settings = _app.get<Settings>(Settings);
+        this._kadence = _app.get<Kadence>(Kadence);
     }
 
-    execute(context: any, args: any, callback: any): void {
-        API.post({
-            uri: '/actor/wallet/reward',
-            json: {
-                identifier: args.identifier,
-                password: args.password,
-                address: args.address,
-                amount: args.amount
-            },
-        }, 
-        this._settings, 
-        function (err, res) {
-            if (err) {
-                context.log(err.body);
-            }
-
-            if (res) {
-                context.log(res.body);
-            }
-
-            callback();
+    async execute(context: any, args: any, callback: any): Promise<void> {
+        const messageEntity = new MessageEntity().add(args.identifier, { 
+            apiVersion: this._settings.ApiVersion,
+            identifier: args.identifier,
+            address: args.address,
+            amount: args.amount,
+            swheKeypair: []
+        }, {
+            topic: Topic.REWARD,
+            members: [new MemberEntity().add(this._settings.Hostname, this._settings.Port, this._settings.HostIdentity)]
         });
+
+        try {
+            const result = await this._kadence.send(Topic.WALLET, messageEntity);
+            // return the reward block hash..
+            context.log(result);
+
+        } catch (err) {
+            context.log(err);
+        }
+
+        callback();
     }
 }
