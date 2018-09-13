cec7cb1517af08f21fb221649fdd19b20d46a3ed
Tangram ($TGM) <dev@getsneak.org>
2018-09-13 10:17:53 +0200

    Added wallet new address

--- src/commands/wallet/address.command.ts
+++ src/commands/wallet/address.command.ts
@@ -6,12 +6,16 @@ import Agent = require('socks5-http-client/lib/Agent');
 import { API } from "../../common/utils/api";
 import { ModuleRef } from "@nestjs/core";
 import { Settings } from "../../common/config/settings.service";
+import { Kadence } from "../../kadence/kadence.service";
+import { MessageEntity } from "../../common/database/entities/message.entity";
+import { Topic } from "../../common/enums/topic.enum";
+import { MemberEntity } from "../../common/database/entities/member.entity";
 
 
 export class WalletAddressCommand extends Command {
     public register(vorpal: any): void {
         var self = this;
-        vorpal.command('wallet address <identifier> <password>', 'Add new address')
+        vorpal.command('wallet address', 'Add new address')
             .action(function (args, cb) {
                 self.execute(this, args, cb);
             });
@@ -20,30 +24,29 @@ export class WalletAddressCommand extends Command {
 
 export class WalletAddressReceiver implements IReceiver {
     private _settings: Settings;
+    private _kadence: Kadence;
 
     constructor(private readonly _app: ModuleRef) {
         this._settings = _app.get<Settings>(Settings);
+        this._kadence = _app.get<Kadence>(Kadence);
     }
 
-    execute(context: any, args: any, callback: any): void {
-        API.post({
-            uri: '/actor/wallet/address',
-            json: {
-                identifier: args.identifier,
-                password: args.password
-            },
-        }, 
-        this._settings, 
-        function (err, res) {
-            if (err) {
-                context.log(err.body);
-            }
-
-            if (res) {
-                context.log(res.body);
-            }
-
-            callback();
+    async execute(context: any, args: any, callback: any): Promise<void> {
+        const messageEntity = new MessageEntity().add(this._settings.Identity, this._settings.ApiVersion, {
+            topic: Topic.ADDRESS,
+            members: [new MemberEntity().add(this._settings.Hostname, this._settings.Port, this._settings.HostIdentity)]
         });
+
+        try {
+            const result = await this._kadence.send(Topic.WALLET, messageEntity);
+
+            // The return JSON[] is added to the wallet..
+            context.log(result);
+
+        } catch (err) {
+            context.log(err);
+        }
+
+        callback();
     }
 }
