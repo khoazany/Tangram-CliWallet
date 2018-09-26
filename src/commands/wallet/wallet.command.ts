c776a98994a2c21bd7eb35e0dcdc00c254ae13cb
Jonathan Bastnagel <j.bastnagel@gmail.com>
2018-09-25 19:13:06 -0500

    Fix wallet fetch command

--- src/commands/wallet/wallet.command.ts
+++ src/commands/wallet/wallet.command.ts
@@ -6,12 +6,13 @@ import request = require('request');
 import Agent = require('socks5-http-client/lib/Agent');
 import { API } from "../../common/utils/api";
 import { INestApplicationContext } from "@nestjs/common";
+import { Vault } from "../../vault/vault.service";
 
 export class WalletCommand extends Command {
     public register(vorpal: any): void {
         var self = this;
         vorpal.command('wallet <identifier> <password>', 'Get wallet')
-            .types({string: ['_']})
+            .types({ string: ['_'] })
             .action(function (args, cb) {
                 self.execute(this, args, cb);
             });
@@ -20,30 +21,21 @@ export class WalletCommand extends Command {
 
 export class WalletReceiver implements IReceiver {
     private _settings: Settings;
+    private _vault: Vault;
 
     constructor(private readonly _app: INestApplicationContext) {
         this._settings = _app.get<Settings>(Settings);
+        this._vault = _app.get<Vault>(Vault);
     }
 
-    execute(context: any, args: any, callback: any): void {
-        API.post({
-            uri: '/actor/wallet',
-            json: {
-                identifier: args.identifier,
-                password: args.password
-            }
-        }, 
-        this._settings, 
-        function (err, res) {
-            if (err) {
-                context.log(err.body);
-            }
+    async execute(context: any, args: any, callback: any): Promise<void> {
+        try {
+            let wallet = await this._vault.getWalletData(args.identifier, args.password);
+            context.log(JSON.stringify(wallet));
+        } catch (err) {
+            context.log(err);
+        }
 
-            if (res) {
-                context.log(res.body);
-            }
-
-            callback();
-        });
+        callback();
     }
 }
